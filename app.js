82a2facb899fcf4aba6c6d955e934a641e381efa
Arman Patel <armanp384@gmail.com>
2023-02-15 12:34:26 +0530

    Level 6 - OAuth and third party Login

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ const mongoose = require("mongoose");
 const session = require("express-session"); // step 1
 const passport = require("passport"); // step 2
 const passportLocalMongoose = require("passport-local-mongoose"); // step 3
+// OAuth 
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const GitHubStrategy = require("passport-github2").Strategy
+const findOrCreate = require("mongoose-findorcreate")
+
 
 const app = express();
 
@@ -34,9 +39,13 @@ mongoose.connect("mongodb://127.0.0.1:27017/userDB");
 const userSchema = new mongoose.Schema({
   email: String,
   password: String,
+  googleId: String,
+  githubId: String
 });
 
 userSchema.plugin(passportLocalMongoose); // step 7
+// OAuth
+userSchema.plugin(findOrCreate)
 // userSchema.plugin(encrypt, {
 //   secret: process.env.SECRET,
 //   encryptedFields: ["password"],
@@ -50,12 +59,67 @@ const LocalStrategy = require("passport-local").Strategy; // step 8
 passport.use(new LocalStrategy(User.authenticate())); // step 9
 
 // use static serialize and deserialize of model for passport session support
-passport.serializeUser(User.serializeUser()); // step 10
-passport.deserializeUser(User.deserializeUser()); // step 11
+passport.serializeUser(function(user, done) {
+    done(null, user.id);                            // Step 10
+  });
+  
+  passport.deserializeUser(function(id, done) {      // Step 11
+    User.findById(id, function(err, user) {
+      done(err, user);                            
+    });
+  });
+// OAuth
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+
+passport.use(new GitHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: "http://127.0.0.1:3000/auth/github/secrets"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    User.findOrCreate({ githubId: profile.id }, function (err, user) {
+      return done(err, user);
+    });
+  }
+));
 
 app.get("/", function (req, res) {
   res.render("home");
 });
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ["profile"] }));
+
+  app.get("/auth/google/secrets", 
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/secrets");
+  });
+
+  app.get("/auth/github",
+  passport.authenticate("github", { scope: [ "user:email" ] }));
+
+app.get("/auth/github/secrets", 
+  passport.authenticate("github", { failureRedirect:" /login "}),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/secrets");
+  });
+
 app.get("/login", function (req, res) {
   res.render("login");
 });
